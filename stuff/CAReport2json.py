1d3a37632afd77c66a27051f8d6a2e8053a3ac91
James Edington <james@ishygddt.xyz>
2020-09-22 03:05:44 -0500

    Prune toolchain a bit

--- stuff/CAReport2json.py
+++ stuff/CAReport2json.py
@@ -4,9 +4,9 @@ import sys,csv,json
 # https://wiki.mozilla.org/CA/Included_Certificates
 # https://ccadb-public.secure.force.com/mozilla/IncludedCACertificateReportCSVFormat
 
-#four lines of code
+#three lines of code
 #this script is nearly objective
 with open('IncludedCACertificateReportCSVFormat') as f:
 	CACertificateReport=list(csv.DictReader(f))
-with open('IncludedCACertificateReportJSONFormat','w') as f:
-	json.dump(CACertificateReport,f,indent=2)
+
+json.dump(CACertificateReport,sys.stdout,indent=2)
