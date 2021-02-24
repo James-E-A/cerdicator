3e66e1be0e7dbf5217a8817358fc2d6fd1bdccda
James <codegeek98+github@gmail.com>
2021-02-24 13:55:23 -0600

    Toolchain: tweak CAReport2json.py

--- stuff/CAReport2json.py
+++ stuff/CAReport2json.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-import sys,csv,json
+import sys, csv, json
 
 # https://wiki.mozilla.org/CA/Included_Certificates
-# https://ccadb-public.secure.force.com/mozilla/IncludedCACertificateReportCSVFormat
+"""Usage:
+curl https://ccadb-public.secure.force.com/mozilla/IncludedCACertificateReportCSVFormat \
+| python3 CAReport2json.py \
+> IncludedCACertificateReport.json
+"""
 
-#three lines of code
-#this script is nearly objective
-with open('IncludedCACertificateReportCSVFormat') as f:
-	CACertificateReport=list(csv.DictReader(f))
-
-json.dump(CACertificateReport,sys.stdout,indent=2)
+# two lines of code
+# this script is nearly objective
+CACertificateReport = list(csv.DictReader(sys.stdin))
+json.dump(CACertificateReport, sys.stdout, indent=2)
