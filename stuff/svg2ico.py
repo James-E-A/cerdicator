a9030f5108c5a3a502a726427fbbdcae39181388
James Edington <james@ishygddt.xyz>
2021-05-23 20:07:04 -0500

    ehh

--- stuff/svg2ico.py
+++ stuff/svg2ico.py
@@ -1,15 +1,14 @@
-from PIL import Image
 import cairosvg.surface
+from PIL import Image
 from io import BytesIO
 
 
-def svg2ico(*args, resolutions={128, 96, 64, 48, 32, 16}, write_to=None, **kwargs):
+def svg2ico(*args, resolutions={128, 96, 64, 48, 32, 24, 16}, write_to=None, **kwargs):
 	t = cairosvg.surface.Tree(*args, **kwargs)
 	ims = []
 	maxres = max(resolutions) if (None not in resolutions) else None
 	for res in resolutions:
-		f = BytesIO()
-		cairosvg.surface.PNGSurface(t, f, 96,
+		cairosvg.surface.PNGSurface(t, f:=BytesIO(), 96,
 		  output_width=res, output_height=res).finish()
 		f.seek(0)
 		if res is not maxres:
@@ -17,11 +16,10 @@ def svg2ico(*args, resolutions={128, 96, 64, 48, 32, 16}, write_to=None, **kwarg
 		else:
 			im = Image.open(f)
 
-	if write_to:
-		return im.save(write_to, format='ICO', append_images=ims)
-	else:
-		f = BytesIO()
-		im.save(f, format='ICO', append_images=ims)
+	# TODO https://github.com/mate-desktop/eom/issues/310
+	# TODO https://github.com/python-pillow/Pillow/issues/2512
+	return im.save(f:=write_to or BytesIO(), format='ICO', append_images=ims)
+	if not write_to:
 		return f.get_value()
 
 if __name__ == '__main__':
