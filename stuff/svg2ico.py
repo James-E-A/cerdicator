8f760b9cc57368ef0cfc8ad7b0b6d52629b76771
James Edington <james@ishygddt.xyz>
2021-05-30 07:05:27 -0500

    Few more tweaks to icons

--- stuff/svg2ico.py
+++ stuff/svg2ico.py
@@ -3,21 +3,20 @@ from PIL import Image  # https://python-pillow.org/
 from io import BytesIO
 
 
-def svg2ico(*args, resolutions={None, 128, 96, 64, 48, 32, 24, 16}, write_to=None, dpi=96, **kwargs):
-	maxres = max(resolutions, key=(lambda res: float('inf') if res is None else res))
+def svg2ico(*args, resolutions={96, 48, 32, 24, 16}, write_to=None, **kwargs):
 	t = cairosvg.surface.Tree(*args, **kwargs)
 	ims = []
 	for res in resolutions:
-		cairosvg.surface.PNGSurface(t, f:=BytesIO(), dpi,
+		cairosvg.surface.PNGSurface(t, f:=BytesIO(), res * 2,
 		  output_width=res, output_height=res).finish()
 		f.seek(0)
 		ims.append(Image.open(f))
-		if res is maxres:
-			im = ims.pop()
+	im = ims.pop(ims.index(max(ims, key=lambda im: sum(im.size))))
 
+	# TODO Fix for MATE Desktop and Windows XP users
 	# https://github.com/mate-desktop/eom/issues/310
-	# TODO https://github.com/python-pillow/Pillow/issues/2512
-	return im.save(f:=(write_to or BytesIO()), format='ICO', append_images=ims)
+	# https://github.com/python-pillow/Pillow/issues/2512
+	im.save(f:=write_to or BytesIO(), format='ICO', append_images=ims, sizes=[im.size for im in [im] + ims])
 	if not write_to:
 		return f.get_value()
 
