b9c70ad95d3b12d35775cb2e4b20e98a311bda85
James Edington <james@ishygddt.xyz>
2022-03-30 18:16:52 -0500

    Add "The Ministry of Digital Development and Communications"
    It's being added as a known-but-not-OOTB-trusted root (the same
    level of "trust" as the US FPKI or CAcert's roots -- that is to
    say, not any inherently, only ensuring that the country displays
    properly)

--- src/js/constants.js
+++ src/js/constants.js
@@ -31,7 +31,8 @@ const fp_host_alt = new Map();
 
 //TODO TODO TODO TODO TODO TODO TODO
 fp_host_alt.set('07:ED:BD:82:4A:49:88:CF:EF:42:15:DA:20:D4:8C:2B:41:D7:15:29:D7:C9:00:F5:70:92:6F:27:7C:C2:30:C5', 'cacert.org');
-fp_host_alt.set('89:4E:BC:0B:23:DA:2A:50:C0:18:6B:7F:8F:25:EF:1F:6B:29:35:AF:32:A9:45:84:EF:80:AA:F8:77:A3:A0:6E', 'gov');  // fpki.gov
+fp_host_alt.set('89:4E:BC:0B:23:DA:2A:50:C0:18:6B:7F:8F:25:EF:1F:6B:29:35:AF:32:A9:45:84:EF:80:AA:F8:77:A3:A0:6E', 'us');  // fpki.gov
+fp_host_alt.set('D2:6D:2D:02:31:B7:C3:9F:92:CC:73:85:12:BA:54:10:35:19:E4:40:5D:68:B5:BD:70:3E:97:88:CA:8E:CF:31', 'ru');  // gosuslugi.ru
 
 // Record the Fingerprint -> (reduced) hostname mappings
 // As well as non-idiopathic Fingerprint -> country mappings
